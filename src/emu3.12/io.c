f3bb10c883186aff70558ec4371eff8ae440484c
takatori <takatorisatoshi@team-lab.com>
2017-01-15 22:33:33 +0900

    io.h io.c 追加

---
+++ src/emu3.12/io.c
@@ -0,0 +1,21 @@
+#include "io.h"
+
+#include <stdio.h>
+#include "emulator.h"
+
+uint8_t io_int8(uint16_t address) {
+  switch (address) {
+  case 0x03f8:
+    return getchar();
+  default:
+    return 0;
+  }
+}
+
+void io_out8(uint16_t address, uint8_t value) {
+  switch(address) {
+  case 0x03f8:
+    putchar(value);
+    break;
+  }
+}
